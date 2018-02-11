0302c0ee5d6fb8c6e80b0b5d35af55d9663d9501
Jwuthri <julien.wut@gmail.com>
2018-02-11 19:26:50 +0100

    Upd train model

--- bitcoinpred/models/train_model.py
+++ bitcoinpred/models/train_model.py
@@ -41,13 +41,24 @@ class TrainLSTM(object):
 
     @staticmethod
     def show_price_evolution(df):
-        """."""
+        """Plot the bitcoin values over the time.
+
+            Arg:
+                df (pd.DataFrame): data
+        """
         btc_trace = go.Scatter(x=df['stamp'], y=df["price"], name="Price")
         py.iplot([btc_trace])
 
     @staticmethod
     def replace_0(df):
-        """."""
+        """Clean NaN values.
+            
+            Arg:
+                df (pd.DataFrame): data
+
+            Return:
+                df (pd.DataFrame): data cleaned
+        """
         df["price"].replace(0, np.nan, inplace=True)
         df["price"].fillna(method="ffill", inplace=True)
 
@@ -55,7 +66,15 @@ class TrainLSTM(object):
 
     @staticmethod
     def scale(df):
-        """."""
+        """Scale the price.
+        
+            Arg:
+                df (pd.DataFrame): data
+
+            Returns:
+                scaled (array): data scaled
+                scaler (MinMaxScaler obj): scaler
+        """
         values = df["price"].values.reshape(-1, 1)
         values = values.astype("float32")
         scaler = MinMaxScaler(feature_range=(0, 1))
@@ -63,31 +82,64 @@ class TrainLSTM(object):
         return scaler.fit_transform(values), scaler
 
     def split_data(self, scaled):
-        """."""
+        """Split the data in train/test.
+
+            Arg:
+                scaled (array): scaled
+
+            Returns:
+                train (array): train data
+                test (array): test data
+                train_size (int): split size
+        """
         train_size = int(len(scaled) * self.split)
         test_size = len(scaled) - train_size
         train, test = scaled[0: train_size, :], scaled[train_size: len(scaled), :]
-        split = train_size
 
         return train, test, train_size
 
-    def create_dataset(self, dataset, look_back, sentiment):
-        """."""
+    @staticmethod
+    def create_dataset(dataset, look_back):
+        """Split dataset into X and Y.
+
+            Arg:
+                dataset (array): data
+                look_back (int): shift
+
+            Returns:
+                X (array): X
+                y (array): y
+        """
         dataX, dataY = [], []
         for i in range(len(dataset) - look_back):
             res = dataset[i: (i + look_back), 0]
-            np.append(res, sentiment[i])
             dataX.append(res)
             dataY.append(dataset[i + look_back, 0])
 
         return np.array(dataX), np.array(dataY)
 
-    def reshape_X(self, df):
-        """."""
+    @staticmethod
+    def reshape_X(df):
+        """Reshape the values into 0 & 1.
+
+            Arg:
+                df (array): data
+
+            Returns:
+                X (array): X reshape
+        """
         return np.reshape(df, (df.shape[0], 1, df.shape[1]))
 
-    def build_model(self, trainX):
-        """."""
+    @staticmethod
+    def build_model(trainX):
+        """Build lstm model.
+
+            Arg:
+                trainX (array): data
+
+            Returns:
+                model (keras.lstm): model compiled
+        """
         model = Sequential()
         model.add(LSTM(100, input_shape=(trainX.shape[1], trainX.shape[2])))
         model.add(Dense(1))
@@ -95,139 +147,238 @@ class TrainLSTM(object):
 
         return model
 
-    def show_val_loss(self, history):
-        """."""
-        pyplot.plot(history.history["loss"], label="train")
-        pyplot.plot(history.history["val_loss"], label="test")
-        pyplot.legend()
-        pyplot.show()
+    @staticmethod
+    def build_model2(trainX):
+        """Build lstm model.
 
-    def show_pred_real(self, real, pred):
-        """."""
-        pyplot.plot(pred, label="pred")
-        pyplot.plot(real, label="real")
-        pyplot.legend()
-        pyplot.show()
+            Arg:
+                trainX (array): data
 
-    def inverse_scale(self, scaler, val):
-        """."""
+            Returns:
+                model (keras.lstm): model compiled
+        """
+        model = Sequential()
+        model.add(LSTM(100, input_shape=(trainX.shape[1], trainX.shape[2]), return_sequences=True))
+        model.add(LSTM(100))
+        model.add(Dense(1))
+        model.compile(loss='mae', optimizer='adam')
+
+        return model
+
+    @staticmethod
+    def show_val_loss(history):
+        """Show learning over the epochs.
+
+            Arg:
+                history (keras.lstm): model evolution
+        """
+        train = go.Scatter(y=history.history["loss"], name="train")
+        test = go.Scatter(y=history.history["val_loss"], name="test")
+        py.iplot([train, test])
+
+    @staticmethod
+    def show_pred_real(real, pred):
+        """Show prediction vs True.
+
+            Args:
+                real (array): real values
+                pred (array): predicted values
+        """
+        real = pd.DataFrame(real, columns=["real"])
+        pred = pd.DataFrame(pred, columns=["pred"])
+        x = go.Scatter(y=pred["pred"], name="pred")
+        y = go.Scatter(y=real["ream"], name="real")
+        py.iplot([y, x])
+
+    @staticmethod
+    def inverse_scale(scaler, val):
+        """Reshape to original values.
+
+            Args:
+                scaler (MinMax obj): scaler
+                val (array): values to reshape
+
+            Return:
+                reshaped (array): values reshaped
+        """
         return scaler.inverse_transform(val.reshape(-1, 1))
 
-    def reshape_Y(self, df):
-        """."""
+    @staticmethod
+    def reshape_Y(df):
+        """Reshape the values into 0 & 1.
+
+            Arg:
+                df (array): data
+
+            Returns:
+                y (array): y reshape
+        """
         return df.reshape(len(df))
 
-    def show_by_date(self, predDates, real, pred):
-        """."""
+    @staticmethod
+    def show_by_date(predDates, real, pred):
+        """Show perdiction over the dates.
+
+            Args:
+                predDates (array): list of dates
+                real (array): real values
+                pred (array): predicted values
+        """
         actual = go.Scatter(x=predDates, y=real, name="Actual")
         pred = go.Scatter(x=predDates, y=pred, name="Pred")
         py.iplot([pred, actual])
 
-    def series_to_supervised(self, data, n_in=1, n_out=1, dropnan=True):
-        n_vars = 1 if type(data) is list else data.shape[1]
-        df = pd.DataFrame(data)
-        cols, names = list(), list()
-        # input sequence (t-n, ... t-1)
-        for i in range(n_in, 0, -1):
-            cols.append(df.shift(i))
-            names += [('var%d(t-%d)' % (j+1, i)) for j in range(n_vars)]
-        # forecast sequence (t, t+1, ... t+n)
-        for i in range(0, n_out):
-            cols.append(df.shift(-i))
-            if i == 0:
-                names += [('var%d(t)' % (j+1)) for j in range(n_vars)]
-            else:
-                names += [('var%d(t+%d)' % (j+1, i)) for j in range(n_vars)]
-        # put it all together
-        agg = pd.concat(cols, axis=1)
-        agg.columns = names
-        # drop rows with NaN values
-        if dropnan:
-            agg.dropna(inplace=True)
-        return agg
+    @staticmethod
+    def show_corr(df):
+        """Show correlation of columns.
+
+            Arg:
+                df (pd.DataFrame): data
+        """
+        data = go.Heatmap(
+            z=df.corr().values.tolist(),
+            x=list(df.corr().columns),
+            y=list(df.corr().index)
+        ) 
+        py.iplot([data])
+
+    @staticmethod
+    def series_to_supervised(scaled):
+        """Split the data in train/test.
+
+            Arg:
+                scaled (pd.DataFrame): scaled
+
+            Return:
+                df_scaled (pd.DataFrame): data with shifted columns
+        """
+        df_scaled = pd.DataFrame(scaled, columns=["var1(t-1)"])
+        df_scaled["var1(t)"] = df_scaled.shift(-1)
+
+        return df_scaled.dropna()
+
+    def create_dataset_sentiment(dataset, look_back, sentiment):
+        """Split dataset into X and Y.
+
+            Arg:
+                dataset (array): data
+                look_back (int): shift
+
+            Returns:
+                X (array): X
+                y (array): y
+        """
+        dataX, dataY = [], []
+        for i in range(len(dataset) - look_back):
+            if i >= look_back:
+                a = dataset[i - look_back: i + 1, 0].tolist()
+                a.append(sentiment[i].tolist()[0])
+            dataX.append(a)
+            dataY.append(dataset[i + look_back, 0])
+
+        return np.array(dataX), np.array(dataY)
 
     def fit_transform(self):
-        """."""
+        """need to be cleaned, REALLY."""
         df = self.read_file()
         df.dropna(axis=1, how="all")
         df["stamp"] = pd.to_datetime(df["stamp"].apply(str), format="%Y%m%d")
         self.show_price_evolution(df)
+
         df = self.replace_0(df)
         self.show_price_evolution(df)
+
         scaled, scaler = self.scale(df)
         sentiment = df["sentiment"].values.reshape(-1, 1).astype("float32")
+        
         train, test, train_size = self.split_data(scaled)
         trainX, trainY = self.create_dataset(train, 1, sentiment[0: train_size])
         testX, testY = self.create_dataset(test, 1, sentiment[train_size:])
+        
         trainX = self.reshape_X(trainX)
         testX = self.reshape_X(testX)
+        
         model = self.build_model(trainX)
         history = model.fit(trainX, trainY, epochs=150, batch_size = 64, validation_data=(testX, testY), verbose=1, shuffle=False)
         self.show_val_loss(history)
+        
         pred = model.predict(testX)
         self.show_pred_real(testY, pred)
+
         pred_inv = self.inverse_scale(scaler, pred)
         real_inv = self.inverse_scale(scaler, testX)
         rmse = sqrt(mean_squared_error(real_inv, pred_inv))
         print("test", rmse)
         self.show_pred_real(real_inv, pred_inv)
+
         predDates = df.tail(len(testX))["stamp"]
         real_inv_resh = self.reshape_Y(real_inv)
         pred_inv_resh = self.reshape_Y(pred_inv)
         self.show_by_date(predDates, real_inv_resh, pred_inv_resh)
-        sns.heatmap(df.corr(), annot=True, cmap="RdYlGn", linewidths=0.1, vmin=0)
-        pyplot.show()
 
-        values = df[["price"]].values
-        values = values.astype("float32")
-        scaler = MinMaxScaler(feature_range=(0, 1))
-        scaled = scaler.fit_transform(values)
-        reframed = self.series_to_supervised(scaled, 1, 1)
+        self.show_corr(df)
+        scaled, scaler = self.scale(df)
+        reframed = self.series_to_supervised(scaled)
 
-        values = reframed.values
-        n_train_hours = int(len(values) * 0.7)
-        train = values[:n_train_hours, :]
-        test = values[n_train_hours:, :]
+        train, test, train_size = self.split_data(reframed.values)
         train_X, train_y = train[:, :-1], train[:, -1]
         test_X, test_y = test[:, :-1], test[:, -1]
         train_X = train_X.reshape((train_X.shape[0], 1, train_X.shape[1]))
         test_X = test_X.reshape((test_X.shape[0], 1, test_X.shape[1]))
 
-        multi_model = Sequential()
-        multi_model.add(LSTM(100, input_shape=(train_X.shape[1], train_X.shape[2])))
-        multi_model.add(Dense(1))
-        multi_model.compile(loss='mae', optimizer='adam')
-        multi_history = multi_model.fit(train_X, train_y, epochs=150, batch_size=100, validation_data=(test_X, test_y), verbose=1, shuffle=False)
+        multi_model = self.build_model(train_X)
+        multi_history = multi_model.fit(trainX, trainY, epochs=150, batch_size = 64, validation_data=(test_X, test_y), verbose=1, shuffle=False)
+        self.show_val_loss(multi_history)
 
-        pyplot.plot(multi_history.history['loss'], label='multi_train')
-        pyplot.plot(multi_history.history['val_loss'], label='multi_test')
-        pyplot.legend()
-        pyplot.show()
-
-        yhat = multi_model.predict(test_X)
-        pyplot.plot(yhat, label='predict')
-        pyplot.plot(test_y, label='true')
-        pyplot.legend()
-        pyplot.show()
+        multi_pred = model.predict(test_X)
+        self.show_pred_real(test_y, multi_pred)
 
         test_X = test_X.reshape((test_X.shape[0], test_X.shape[1]))
-        inv_yhat = np.concatenate((yhat, test_X[:, 1:]), axis=1)
-        inv_yhat = scaler.inverse_transform(inv_yhat)
-        inv_yhat = inv_yhat[:,0]
+        inv_multi_pred = np.concatenate((multi_pred, test_X[:, 1:]), axis=1)
+        inv_multi_pred = scaler.inverse_transform(inv_multi_pred)
+        inv_multi_pred = inv_multi_pred[:, 0]
+
         test_y = test_y.reshape((len(test_y), 1))
-        inv_y = np.concatenate((test_y, test_X[:, 1:]), axis=1)
-        inv_y = scaler.inverse_transform(inv_y)
-        inv_y = inv_y[:,0]
+        inv_multi_real = np.concatenate((test_y, test_X[:, 1:]), axis=1)
+        inv_multi_real = scaler.inverse_transform(inv_multi_real)
+        inv_multi_real = inv_multi_real[:, 0]
 
-        rmse = sqrt(mean_squared_error(inv_y, inv_yhat))
-        print('Test RMSE: %.3f' % rmse)
+        rmse = sqrt(mean_squared_error(inv_multi_real, inv_multi_pred))
+        print("test", rmse)
 
-        actual_chart = go.Scatter(x=predDates, y=inv_y, name= 'Actual Price')
-        multi_predict_chart = go.Scatter(x=predDates, y=inv_yhat, name= 'Multi Predict Price')
+        actual_chart = go.Scatter(x=predDates, y=inv_multi_real, name= 'Actual Price')
+        multi_predict_chart = go.Scatter(x=predDates, y=inv_multi_pred, name= 'Multi Predict Price')
         predict_chart = go.Scatter(x=predDates, y=pred_inv_resh, name= 'Predict Price')
         py.iplot([predict_chart, multi_predict_chart, actual_chart])
 
+        reframed["sentiment"] = df["sentiment"].head(-1)
+        train, test, train_size = self.split_data(reframed.values)
 
+        trainX, trainY = create_dataset(train, 1, sentiment[:train_size],sent=True)
+        testX, testY = create_dataset(test, 1, sentiment[train_size:], sent=True)
+        trainX = np.reshape(trainX, (trainX.shape[0], 1, trainX.shape[1]))
+        testX = np.reshape(testX, (testX.shape[0], 1, testX.shape[1]))
+
+        multi_model2 = self.build_model2(train_X)
+        multi_history2 = multi_model2.fit(trainX, trainY, epochs=150, batch_size = 64, validation_data=(testX, testY), verbose=1, shuffle=False)
+        self.show_val_loss(multi_history2)
+
+        multi_pred2 = model.predict(testX)
+        self.show_pred_real(testY, multi_pred2)
+
+        pred_inverse_sent = scaler.inverse_transform(multi_pred2.reshape(-1, 1))
+        testY_inverse_sent = scaler.inverse_transform(testY.reshape(-1, 1))
+
+        rmse_sent = sqrt(mean_squared_error(testY_inverse_sent, pred_inverse_sent))
+        print('Test RMSE: %.3f' % rmse_sent)
+
+        a = pd.DataFrame(pred_inverse_sent, columns=["a"])
+        b = pd.DataFrame(testY_inverse_sent, columns=["a"])
+        actual_chart = go.Scatter(x=predDates, y=a["a"], name= 'Actual Price')
+        multi_predict_chart = go.Scatter(x=predDates, y=b["a"], name= 'Multi Predict Price')
+        py.iplot([multi_predict_chart, actual_chart])
+
+        
 if __name__ == '__main__':
     path = os.path.join(converged_data_path, "merged.csv")
     TrainLSTM(path).fit_transform()
